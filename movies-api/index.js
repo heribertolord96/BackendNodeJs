f18cc38709b7e28c00150435df5b8b89e9b79c81
Heriberto <heribertolord@gmail.com>
2020-09-29 22:03:41 -0500

    working at home

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,44 +1,14 @@
-/* const express = require('express');
-const app = express();
-
-const { config }= require('./config/index');
-
-app.get('/', function(req,res){
-    res.send('hello world');
-
-});
-app.get('/json', function(req, res){
-    res.json({hello: "world"});
-});
-
-app.listen(config.port, function(){
-    console.log(`Listening http://localhost:${config.port}`);
-}); */
-
 const express = require('express');
-
 const app = express();
 
 const { config } = require('./config/index');
-const moviesApi = require('./routes/movies.js')
+const moviesApi = require('./routes/movies.js');
 
-/* app.get('/', function(req, res) {
-    res.send('hello world');
-});
+// body parser
+app.use(express.json());
 
-app.get('/json', function(req, res) {
-    res.json({ hello: 'world' });
-});
-
-app.get('/:year', function(req, res) {
-    const year = parseInt(req.params.year);
-    if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
-        res.send(`${year} es bisiesto`);
-    }else{
-        res.send(`${year} no es bisiesto`);
-    }
-}); */
 moviesApi(app);
+
 app.listen(config.port, function() {
-    console.log(`Listening http://localhost:${config.port}`);    
+  console.log(`Listening http://localhost:${config.port}`);
 });
